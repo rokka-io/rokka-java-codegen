50935a32e95aa9718702784a24eaa0adeb498495
Christian Stocker <me@chregu.tv>
2018-05-03 11:41:47 +0200

    add usermeta example

--- src/main/java/io/rokka/examples/UploadImage.java
+++ src/main/java/io/rokka/examples/UploadImage.java
@@ -1,8 +1,11 @@
 package io.rokka.examples;
 
+import com.google.gson.internal.LinkedTreeMap;
 import io.rokka.client.codegen.ApiException;
 import io.rokka.client.codegen.api.SourceimagesApi;
 import io.rokka.client.codegen.model.ListSourceImagesResponse;
+import io.rokka.client.codegen.model.SourceImage;
+
 import java.io.File;
 
 
@@ -16,10 +19,14 @@ public class UploadImage {
 
         try {
             File image = new File("image.png");
-            ListSourceImagesResponse response = apiInstance.createSourceImage(image, config.getOrganization(), null, null);
+            ListSourceImagesResponse response = apiInstance.createSourceImage(image, config.getOrganization(), null, "{\"foo\": \"bar\"}");
 
             System.out.println(response);
-            System.out.println("\n Hash of new image is: " + response.getItems().get(0).getHash());
+            SourceImage firstImage = response.getItems().get(0);
+            System.out.println("\n Hash of new image is: " + firstImage.getHash());
+            LinkedTreeMap usermeta = (LinkedTreeMap) firstImage.getUserMetadata();
+            System.out.println("\n User metadata is: " + usermeta);
+            System.out.println("\n User metadata foo is: " + usermeta.get("foo"));
         } catch (ApiException e) {
             System.err.println("Exception");
             e.printStackTrace();
